ea9f05c55e43ee79031d1c19283d82c5c1e210fe
dougyb83 <dougy.b@proton.me>
2024-01-04 14:42:06 +0000

    Update env import

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
-from dotenv import load_dotenv
+if os.path.exists("env.py"):
+    import env
+    
+# from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+# # Load environment variables from .env file
+# load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
