3d5b51764f5a9c9d63e7175e3520b01432bfe45b
Doug B <dougy.b@proton.me>
2024-01-05 13:33:14 +0000

    fix verify email

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # DEBUG = 'DEVELOPMENT' in os.environ
 DEBUG = True
 
-ALLOWED_HOSTS = ['boutique-ado-doug.herokuapp.com', 'localhost', '127.0.0.1', 'boutique-ado-doug-0793d3d1f1bc.herokuapp.com']
+ALLOWED_HOSTS = ['boutique-ado-doug.herokuapp.com', 'localhost', '127.0.0.1', 'boutique-ado-doug-0793d3d1f1bc.herokuapp.com', '8000-dougyb83-boutiqueadov1-qls593vasya.ws-eu107.gitpod.io']
 
 
 # Application definition
@@ -115,7 +115,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'none'  # otherwise 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
