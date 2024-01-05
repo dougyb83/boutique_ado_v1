104ff62d25932e5c2482eaf821df6a3bb12ad827
dougyb83 <dougy.b@proton.me>
2024-01-05 07:03:07 +0000

    Debug to True

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -31,7 +31,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['boutique-ado-doug.herokuapp.com', 'localhost', '127.0.0.1', 'boutique-ado-doug-0793d3d1f1bc.herokuapp.com']
 
